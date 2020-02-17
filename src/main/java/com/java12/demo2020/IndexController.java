4065df0d1ba8d144b068c748fd52a27408bf2fd6
mabidar <abidar.moh@gmail.com>
2020-02-17 12:33:52 +0100

    Update IndexController.java

--- src/main/java/com/java12/demo2020/IndexController.java
+++ src/main/java/com/java12/demo2020/IndexController.java
@@ -1,4 +1,4 @@
-package com.example.herokudemo;
+package com.java12.demo2020;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
